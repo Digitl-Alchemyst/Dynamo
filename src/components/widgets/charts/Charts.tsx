a3cd9ceede7c7b89c14d16cae1f704f9cf5bf5b0
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-07-18 21:45:02 -0500

    Data Map for Chart Widgets

--- src/components/widgets/charts/Charts.tsx
+++ src/components/widgets/charts/Charts.tsx
@@ -1,66 +1,32 @@
 import { Link } from 'react-router-dom';
 import './charts.scss';
 import { LineChart, Line, ResponsiveContainer, Tooltip } from 'recharts';
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
-const Charts = () => {
+type Props = {
+    color: string;
+    icon: string;
+    title: string;
+    dataKey: string;
+    number: number | string;
+    percentage: number;
+    chartData: object[];
+};
+
+const Charts = (props: Props) => {
   return (
     <div className="chartBox">
       <div className="boxInfo">
         <div className="title">
-          <img src="/user.svg" alt="" />
-          <span> Sales Team</span>
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
         </div>
-        <h1>65</h1>
-        <Link to="/">View all</Link>
+        <h1>{props.number}</h1>
+        <Link to="/" style={{ color: props.color }}>View all</Link>
       </div>
       <div className="chartInfo">
         <div className="chart">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart width={300} height={100} data={data}>
+          <ResponsiveContainer width="99%" height="100%">
+            <LineChart data={props.chartData}>
               <Tooltip
                 contentStyle={{ background: 'transparent', border: 'none' }}
                 labelStyle={{ display: 'none' }}
@@ -69,8 +35,8 @@ const Charts = () => {
 
               <Line
                 type="monotone"
-                dataKey="pv"
-                stroke="#8884d8"
+                dataKey={props.dataKey}
+                stroke={props.color}
                 strokeWidth={2}
                 dot={false}
               />
@@ -78,7 +44,7 @@ const Charts = () => {
           </ResponsiveContainer>
         </div>
         <div className="texts">
-          <span className="percentage">69%</span>
+          <span className="percentage" style={{ color: props.percentage < 0 ? 'tomato' : 'limegreen' }}>{props.percentage}%</span>
           <span className="duration">This Month</span>
         </div>
       </div>
