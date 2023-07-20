b08c530d170d4ef27e776a16663367e791f8196d
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-07-20 18:25:44 -0500

    Pie Chart Debugged

--- src/components/widgets/pieChart/PieCharts.tsx
+++ src/components/widgets/pieChart/PieCharts.tsx
@@ -1,9 +1,9 @@
-
+import './pieCharts.scss';
 import { ResponsiveContainer, Pie, PieChart, Cell } from 'recharts';
 
 interface PieData {
   name: string;
-  value: string;
+  value: number;
   color: string;
 }
 
@@ -13,37 +13,22 @@ type Props = {
   pieChartData: PieData[];
 };
 
-const data = {
-  title: 'Leads by Source',
-  dataKey: 'name',
-  pieChartData: [
-    { name: 'Social Media', value: '73', color: '#0088FE' },
-    { name: 'Google', value: '83', color: '#00C49F' },
-    { name: 'Craigslist', value: '46', color: '#FFBB28' },
-    { name: 'Direct', value: '122', color: '#FF804E' },
-    { name: 'Other', value: '32', color: '#CC93A3' },
-  ],
-};
-
-const PieCharts = (props: Props) => {
+const PieCharts = (props: Props) => { 
   return (
     <div className="pieChartBox">
-      <h1>{data.title}</h1>
+      <h1>{props.title}</h1> 
       <div className="chart">
         <ResponsiveContainer width="99%" height={300}>
           <PieChart>
             <Pie
-              data={data.pieChartData}
-              cx={120}
-              cy={200}
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
+              data={props.pieChartData} 
+              innerRadius={'70%'}
+              outerRadius={'90%'}
               paddingAngle={5}
-              dataKey="name"
+              dataKey="value"
             >
-              {data.pieChartData.map((data) => (
-                <Cell key={data.name} fill={data.color} />
+              {props.pieChartData.map((item) => ( 
+                <Cell key={item.name} fill={item.color} />
               ))}
             </Pie>
           </PieChart>
--- src/components/widgets/pieChart/pieChartsData.ts
+++ src/components/widgets/pieChart/pieChartsData.ts
@@ -2,10 +2,10 @@ export const pieChartDataLeads = {
   title: 'Leads by Source',
   dataKey: 'name',
   pieChartData: [
-    { name: 'Social Media', value: '73', color: '#0088FE' },
-    { name: 'Google', value: '83', color: '#00C49F' },
-    { name: 'Craigslist', value: '46', color: '#FFBB28' },
-    { name: 'Direct', value: '122', color: '#FF804E' },
-    { name: 'Other', value: '32', color: '#CC93A3' },
+    { name: 'Social Media', value: 73, color: '#0088FE' },
+    { name: 'Google', value: 83, color: '#00C49F' },
+    { name: 'Craigslist', value: 46, color: '#FFBB28' },
+    { name: 'Direct', value: 122, color: '#FF804E' },
+    { name: 'Other', value: 32, color: '#CC93A3' },
   ],
 };
